aeaa037ed2f506766dc6f3e610a4ce80f5daa75e
David Paul Weber <dpweberza@gmail.com>
2013-09-22 22:30:01 +0200

    Configurable css files

--- src/config/config.php
+++ src/config/config.php
@@ -1,7 +1,9 @@
 <?php
 
 return array(
-    'logo' => 'packages/david-weber/dominion/assets/img/shield.png',
-    'title' => 'Dominion',
-    'footer' => 'Powered by Dominion'
+    'asset-logo' => 'packages/david-weber/dominion/assets/img/shield.png',
+    'asset-favicon' => 'packages/david-weber/dominion/assets/img/favicon.ico',
+    'asset-css' => array('packages/david-weber/dominion/assets/css/bootstrap.min.css', 'packages/david-weber/dominion/assets/css/bootstrap-theme.min.css', 'packages/david-weber/dominion/assets/css/admin.css'),
+    'text-title' => 'Dominion',
+    'text-footer' => 'Powered by Dominion'
 );
--- src/views/templates/base.blade.php
+++ src/views/templates/base.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>{{Config::get('dominion::title')}}</title>
+        <title>{{Config::get('dominion::text-title')}}</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="{{URL::asset('packages/david-weber/dominion/assets/css/bootstrap.min.css')}}" rel="stylesheet"></link>
-        <link href="{{URL::asset('packages/david-weber/dominion/assets/css/bootstrap-theme.min.css')}}" rel="stylesheet"></link>
-        <link href="{{URL::asset('packages/david-weber/dominion/assets/css/admin.css')}}" rel="stylesheet"></link>
-        <link href="{{URL::asset('packages/david-weber/dominion/assets/img/favicon.ico')}}" rel="icon" type="image/x-icon"></link>
+        @foreach(Config::get('dominion::asset-css') as $css)
+            <link href="{{URL::asset($css)}}" rel="stylesheet"></link>
+        @endforeach
+        <link href="{{Config::get('dominion::asset-favicon')}}" rel="icon" type="image/x-icon"></link>
     </head>
     <body>
         <div id="wrap">
@@ -19,7 +19,7 @@
                             <span class="icon-bar"></span>
                             <span class="icon-bar"></span>
                         </button>
-                        <a class="navbar-brand" href="{{ action('DavidWeber\Dominion\Controllers\AdminController@getIndex')}}">@if (Config::get('dominion::logo')) <img src="{{URL::asset(Config::get('dominion::logo'))}}" /> @endif  {{Config::get('dominion::title')}}</a>
+                        <a class="navbar-brand" href="{{ action('DavidWeber\Dominion\Controllers\AdminController@getIndex')}}">@if (Config::get('dominion::asset-logo')) <img src="{{URL::asset(Config::get('dominion::asset-logo'))}}" /> @endif  {{Config::get('dominion::text-title')}}</a>
                     </div>
                     <div class="collapse navbar-collapse navbar-ex1-collapse">
                         @yield('navigation')
@@ -32,7 +32,7 @@
         </div>
         <div id="footer">
             <div class="container">
-                <p class="text-muted pull-right">{{Config::get('dominion::footer')}}</p>
+                <p class="text-muted pull-right">{{Config::get('dominion::text-footer')}}</p>
             </div>
         </div>
         <script src="{{URL::asset('packages/david-weber/dominion/assets/js/jquery-1.10.2.min.js')}}"></script>
